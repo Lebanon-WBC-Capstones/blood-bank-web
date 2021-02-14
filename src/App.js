82c53cc6a0ed1ab22090d71b660e097ccbfa22e4
joliehobloss <jolieshobloss@gmail.com>
2021-02-15 01:31:57 +0200

    Install all VSCode extensions required Fixes #2

--- src/App.js
+++ src/App.js
@@ -8,6 +8,7 @@ function App() {
         <img src={logo} className="App-logo" alt="logo" />
         <p>
           Edit <code>src/App.js</code> and save to reload.
+          <h1>Hello world JH</h1>
         </p>
         <a
           className="App-link"
