5b64c61b3b05567316014692064d6ee6dc73ae2f
layla hammoud <laylahammoud63@gmail.com>
2021-02-18 01:16:57 +0200

    Feat: Create the email confirmation component Fixes #30

--- src/App.js
+++ src/App.js
@@ -1,23 +1,10 @@
 import logo from './logo.svg';
 import './App.css';
-
+import Configuration from './components/emailconfiguratin';
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="text-pink"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Configuration />
     </div>
   );
 }
---
+++ src/components/emailconfiguratin.js
@@ -0,0 +1,24 @@
+import React from 'react';
+import signinlogo from '../assets/signinlogo.svg';
+const Configuration = () => {
+  return (
+    <div className="text-gray-500 ">
+      <div className="mt-12">
+        <img
+          src={signinlogo}
+          alt="donation icon"
+          className="block"
+          style={{ margin: '0 auto' }}
+        />
+      </div>
+      <div className="m-7 pt-10">
+        <div className="text-5xl">Thank you </div>
+        <div className="text-5xl">Layla !</div>
+      </div>
+      <div className="mx-10 px-10 pt-10 mt-10">
+        <p className="text-xl">Please check your email to verify the account</p>
+      </div>
+    </div>
+  );
+};
+export default Configuration;
