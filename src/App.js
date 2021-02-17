a9b5faa2c00035438fd3f0d2fb4a88013552e5fb
layla hammoud <laylahammoud63@gmail.com>
2021-02-18 01:54:21 +0200

    Feat: Create the email confirmation component Fixes #30

--- src/App.js
+++ src/App.js
@@ -2,11 +2,7 @@ import logo from './logo.svg';
 import './App.css';
 import Configuration from './components/emailconfiguratin';
 function App() {
-  return (
-    <div className="App">
-      <Configuration />
-    </div>
-  );
+  return <div className="App"></div>;
 }
 
 export default App;
