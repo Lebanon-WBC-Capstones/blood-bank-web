d8077d10780144f0208eb9576aec736f7a90132e
JANAZR9 <janjoune.97@hotmail.com>
2021-02-18 02:30:28 +0200

    Feat: Create the layout component and 3 pages Fixes #27

---
+++ src/components/Layout.js
@@ -0,0 +1,22 @@
+import React from 'react';
+
+function Layout(props) {
+  return (
+    <div>
+      <div className="min-h-screen bg-pri-red py-10 sm:p-8">
+        <div
+          className=" bg-white p-10 flex items-center justify-center"
+          style={{
+            borderStyle: 'solid',
+            borderRadius: '0px 50px 0px 50px',
+            height: '90vh',
+          }}
+        >
+          <div></div>
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default Layout;
