d99d07f2cd4dd6582c8d3434740be63f68ff72c9
JANAZR9 <janjoune.97@hotmail.com>
2021-02-18 17:40:49 +0200

    Feat: Create the layout component and 3 pages Fixes #27

--- src/components/Layout.js
+++ src/components/Layout.js
@@ -5,14 +5,14 @@ function Layout(props) {
     <div>
       <div className="min-h-screen bg-pri-red py-10 sm:p-8">
         <div
-          className=" bg-white p-10 flex items-center justify-center"
+          className=" bg-white p-10 flex items-center justify-center sm:w-full"
           style={{
             borderStyle: 'solid',
             borderRadius: '0px 50px 0px 50px',
             height: '90vh',
           }}
         >
-          <div></div>
+          <div>{props.children}</div>
         </div>
       </div>
     </div>
