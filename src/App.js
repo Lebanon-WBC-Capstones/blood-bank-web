030bbf755e979868314c48bda294adcadd5bef78
Mohamad Tassieh <mohamad.bassam.tessieh@gmail.com>
2021-02-21 21:57:32 +0200

    Feat: Create the back page layout Fixes #43

--- src/App.js
+++ src/App.js
@@ -1,8 +1,12 @@
 import './App.css';
-import Footer from '../src/components/Footer.js';
+import { BrowserRouter as Router } from 'react-router-dom';
 
 function App() {
-  return <div className="App"></div>;
+  return (
+    <Router>
+      <div className="App"></div>
+    </Router>
+  );
 }
 
 export default App;
