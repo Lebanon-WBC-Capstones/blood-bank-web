c4fdcb158b27abf90cf217d68aa852b9f74b9aa9
Wisam Naji <guitar90rocker@gmail.com>
2021-02-24 20:13:14 +0000

    removed unused object

--- src/components/FeedItem.js
+++ src/components/FeedItem.js
@@ -17,24 +17,6 @@ const FeedItem = (props) => {
     return new Date(string).toLocaleDateString([], options);
   };
 
-  const feed = [
-    {
-      fulfilled: true,
-      user: {
-        username: 'Jolie',
-        avatar:
-          'https://user-images.githubusercontent.com/12988551/108382684-1f03f000-721a-11eb-901b-3419f392e548.png',
-      },
-      data: {
-        operation: 'Request',
-        date: '2021-02-18T15:47:40.314Z',
-        reason: 'Car accident',
-        amount: '2 pints',
-        donors: ['Ahmed', 'Layla', 'jana', 'nabi8a'],
-      },
-    },
-  ];
-
   return (
     <div>
       <div
