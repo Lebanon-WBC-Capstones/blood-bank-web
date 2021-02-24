166a4d6c1a41b02c2feec8d70c4a1f9b31b20143
Wisam Naji <guitar90rocker@gmail.com>
2021-02-24 20:23:16 +0000

    reverted files

--- src/App.js
+++ src/App.js
@@ -1,7 +1,12 @@
 import './App.css';
+import { BrowserRouter as Router } from 'react-router-dom';
 
 function App() {
-  return <div className="App"></div>;
+  return (
+    <Router>
+      <div className="App"></div>
+    </Router>
+  );
 }
 
 export default App;
