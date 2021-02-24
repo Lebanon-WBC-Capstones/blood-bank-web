a6915970a0343499759ab8b5850a009c2c5cc11c
joliehobloss <jolieshobloss@gmail.com>
2021-02-24 01:51:34 -0800

    Feat: create the sign in component Fixes #29

--- src/components/SignIn.js
+++ src/components/SignIn.js
@@ -3,23 +3,19 @@ import signInLogo from '../assets/signInLogo.svg';
 
 const SignIn = () => {
   return (
-    <div class="bg-transparent">
-      <div class="mt-12 ">
-        <img
-          src={signInLogo}
-          alt="images"
-          style={{ display: 'block', margin: '0 auto' }}
-        />
+    <div className="bg-transparent container max-width: 640px">
+      <div className="mt-12 ">
+        <img src={signInLogo} alt="images" className="block mx-auto" />
       </div>
 
-      <div class="font-Roboto text-3xl mt-5 text-gray-500">
+      <div className="font-Roboto text-3xl mt-5 text-gray-500">
         <p>Sign In</p>
       </div>
 
-      <div class=" mt-5">
+      <div className=" mt-5">
         <form>
           <div>
-            <label for="email" class="text-gray-500 font-Roboto pr-56">
+            <label for="email" className="text-gray-500 font-Roboto pr-56">
               Email
             </label>
             <br />
@@ -27,12 +23,12 @@ const SignIn = () => {
               type="text"
               id="email"
               name="email"
-              class="  w-3/4 rounded-lg h-9 border-2"
+              className="  w-3/4 rounded-lg h-9 border-2"
             />
           </div>
 
-          <div class="mt-6">
-            <label for="password" class="text-gray-500 font-Roboto pr-48">
+          <div className="mt-6">
+            <label for="password" className="text-gray-500 font-Roboto pr-48">
               Password
             </label>
             <br />
@@ -40,27 +36,27 @@ const SignIn = () => {
               type="password"
               id="password"
               name="password"
-              class="  w-3/4 rounded-lg h-9 border-2"
+              className="  w-3/4 rounded-lg h-9 border-2"
             />
           </div>
 
-          <div class="mt-2 pr-32">
+          <div className="mt-2 pr-32">
             <input
               type="checkbox"
               id="remember_me"
               name="remember_me"
-              class="rounded-lg"
+              className="rounded-lg"
             />
-            <label for="remember_me" class="text-gray-500 font-Roboto ml-2">
+            <label for="remember_me" className="text-gray-500 font-Roboto ml-2">
               Remember Me
             </label>
           </div>
 
-          <div class="mt-32">
+          <div className="mt-32">
             <input
               type="submit"
               value="Sign In"
-              class="bg-pink  w-3/4 rounded-lg h-9 text-gray-500 font-Roboto"
+              className="bg-pink  w-3/4 rounded-lg h-9 text-gray-500 font-Roboto"
             />
           </div>
         </form>
