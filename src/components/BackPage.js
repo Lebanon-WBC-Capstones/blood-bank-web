1d129c105589b0e25cc90f7724d15416e5bc0133
joliehobloss <jolieshobloss@gmail.com>
2021-03-02 01:05:13 +0200

    Feat: Create the feeds page Fixes #75

--- src/components/BackPage.js
+++ src/components/BackPage.js
@@ -8,34 +8,40 @@ function BackPage(props) {
   const history = useHistory();
 
   return (
-    <div className="flex justify-between flex-col bg-white max-width-screen-sm ">
-      <div
-        onClick={() => history.goBack()}
-        className="absolute border-0 mt-10 ml-6 "
-      >
-        <img
-          src={ICBack}
-          alt="button"
-          className="sm:w-full sm:h-full sm:mt-4 sm:ml-3"
-        />
-      </div>
-      <h3 className="absolute text-md font-Roboto ml-16 mt-8 font-medium text-2xl text-red-100 sm:ml-24 sm:mt-12 sm:text-4xl">
-        {props.title}
-      </h3>
-      <div>
-        <img
-          src={TopWave}
-          alt="shape"
-          className="w-full h-min-full sm:w-full"
-        />
-      </div>
-      <div>{props.children}</div>
-      <div style={{ marginTop: '27.5rem' }}>
-        <img
-          src={BottomWave}
-          alt="Bottom"
-          className="w-full h-min-full sm:w-full "
-        />
+    <div>
+      <div className="flex flex-col ">
+        <div className="bg-white w-screen fixed top-0 h-auto">
+          <div
+            onClick={() => history.goBack()}
+            className="absolute border-0 mt-10 ml-6 "
+          >
+            <img
+              src={ICBack}
+              alt="button"
+              className="sm:w-full sm:h-full sm:mt-4 sm:ml-3"
+            />
+          </div>
+          <h3 className="absolute text-md font-Roboto ml-16 mt-8 font-medium text-2xl text-red-100 sm:ml-24 sm:mt-12 sm:text-4xl">
+            {props.title}
+          </h3>
+          <div>
+            <img
+              src={TopWave}
+              alt="shape"
+              className="w-full h-min-full sm:w-full"
+            />
+          </div>
+        </div>
+
+        <div className="mt-36 mb-36">{props.children}</div>
+
+        <div className="bg-white w-screen fixed bottom-0 h-auto">
+          <img
+            src={BottomWave}
+            alt="Bottom"
+            className="w-full h-min-full sm:w-full "
+          />
+        </div>
       </div>
     </div>
   );
