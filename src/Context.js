93f9783c39f941b570f9fd242868003b51266496
Abdulrahman Ghalayini <ghalayini.abdulrahman.17@gmail.com>
2021-03-09 16:54:06 +0200

    Feat: create the context and it's provider and export it then wrap the app by the provider Fixes #103

--- src/Context.js
+++ src/Context.js
@@ -3,6 +3,6 @@ import React, { createContext, useState } from 'react';
 export const TheContext = createContext();
 
 const Context = (props) => {
-  const { user, setUser } = useState('Guest');
+  const { user } = useState('Guest');
   return <Context.Providor value={{ user }}>{props.children}</Context.Providor>;
 };
