62e22b0be128fd7534d0f6bb2c4698765948fdde
Abdulrahman Ghalayini <ghalayini.abdulrahman.17@gmail.com>
2021-03-09 17:04:35 +0200

    done

--- src/Context.js
+++ src/Context.js
@@ -2,7 +2,12 @@ import React, { createContext, useState } from 'react';
 
 export const TheContext = createContext();
 
-export const Context = (props) => {
+// export const Context = (props) => {
+//   const { user } = useState('Guest');
+// };
+
+function Context(props) {
   const { user } = useState('Guest');
   return <Context.Providor value={{ user }}>{props.children}</Context.Providor>;
-};
+}
+export default Context;
