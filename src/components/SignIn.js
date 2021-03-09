d74aba0724e9ab2da20174490c54cae5fdaf2204
nabigha-mogharbel <nabiga.ba.mo1998@gmail.com>
2021-03-09 11:25:38 +0200

    Feat: create the sign in feature using firebase.auth() in the sign in component. Fixes #114

--- src/components/SignIn.js
+++ src/components/SignIn.js
@@ -1,8 +1,31 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import signInLogo from '../assets/signlogo.svg';
 import { Link } from 'react-router-dom';
+//import {auth} from './firebase.config';
 
 const SignIn = () => {
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('');
+  const [displayName, setDisplayName] = useState('');
+  /*useEffect(() => {
+    signIn()
+  }, [email.current.value, password.current.value])*/
+  const handleSubmit = async (e, email, password) => {
+    e.preventDefault();
+    console.log('trtttr');
+  };
+
+  const handleChange = (event) => {
+    const { name, value } = event.currentTarget;
+    if (name === 'userEmail') {
+      setEmail(value);
+    } else if (name === 'userPassword') {
+      setPassword(value);
+    } else if (name === 'displayName') {
+      setDisplayName(value);
+    }
+  };
+
   return (
     <div className="bg-transparent container max-width: 640px">
       <div className="mt-12 ">
@@ -14,14 +37,16 @@ const SignIn = () => {
       </div>
 
       <div className=" mt-5">
-        <form>
+        <form onSubmit={(e) => handleSubmit(e, email, password)}>
           <div>
             <label className="text-gray-500 font-Roboto pr-56">Email</label>
             <br />
             <input
-              type="text"
+              type="email"
               id="email"
-              name="email"
+              name="userEmail"
+              value={email}
+              onChange={handleChange}
               className="  w-3/4 rounded-lg h-9 border-2 focus:outline-none focus:ring-2 focus:ring-gray-400 focus:ring-opacity-50"
             />
           </div>
@@ -32,7 +57,9 @@ const SignIn = () => {
             <input
               type="password"
               id="password"
-              name="password"
+              name="userPassword"
+              value={password}
+              onChange={handleChange}
               className="  w-3/4 rounded-lg h-9 border-2 focus:outline-none focus:ring-2 focus:ring-gray-400 focus:ring-opacity-50"
             />
           </div>
