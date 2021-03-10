e3d1b9748109cadcfc49ba82cec0fd293bb52ccd
Abdulrahman Ghalayini <ghalayini.abdulrahman.17@gmail.com>
2021-03-10 08:42:22 +0200

    done

--- src/Context.js
+++ src/Context.js
@@ -1,6 +1,6 @@
 import React, { createContext, useState } from 'react';
 
-export const TheContext = createContext();
+Context = createContext();
 
 // export const Context = (props) => {
 //   const { user } = useState('Guest');
