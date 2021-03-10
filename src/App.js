4dd7b4f77170379afabd00306eb4eddd17864d1a
Abdulrahman Ghalayini <ghalayini.abdulrahman.17@gmail.com>
2021-03-10 08:49:33 +0200

    done

--- src/App.js
+++ src/App.js
@@ -16,7 +16,7 @@ import UrgentPage from './pages/UrgentPage.js';
 import DonatePage from './pages/DonationRequestConfirmationPage.js';
 import DonateAppointment from './pages/DonateAppointment.js';
 import VolunteerPage from './pages/VolunteerPage.js';
-import Context from './Context';
+import ContextProvidor from './Context';
 
 /*
 import DonateConfirm from './pages/DonationConfirmPage.js';
@@ -25,7 +25,7 @@ import DonateRejected from './pages/DonationRejectedPage.js';
 
 function App() {
   return (
-    <Context>
+    <ContextProvidor>
       <Router>
         <div className="App">
           <Switch>
@@ -71,7 +71,7 @@ function App() {
           </Switch>
         </div>
       </Router>
-    </Context>
+    </ContextProvidor>
   );
 }
 
--- src/Context.js
+++ src/Context.js
@@ -1,13 +1,13 @@
 import React, { createContext, useState } from 'react';
 
-Context = createContext();
+export const Context = createContext();
 
 // export const Context = (props) => {
 //   const { user } = useState('Guest');
 // };
 
-function Context(props) {
+function ContextProvidor(props) {
   const { user } = useState('Guest');
   return <Context.Providor value={{ user }}>{props.children}</Context.Providor>;
 }
-export default Context;
+export default ContextProvidor;
