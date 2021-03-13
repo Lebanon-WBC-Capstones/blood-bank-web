f732cd2ad502a236b56a932063dfda20b72fbff7
JANAZR9 <janjoune.97@hotmail.com>
2021-03-13 23:37:33 +0200

    fix: tweak all ui to dazzle Fixes #126

--- src/pages/AboutUsPage.js
+++ src/pages/AboutUsPage.js
@@ -4,9 +4,7 @@ import AboutGraph from '../components/AboutGraph';
 import TeamCircles from '../components/TeamCircles';
 import ContactUs from '../components/ContactUs';
 import AboutFooter from '../components/AboutFooter';
-
-import { withNamespaces } from 'react-i18next';
-function AboutUsPage({ t }) {
+function AboutUsPage() {
   return (
     <>
       <AboutLayout>
@@ -23,13 +21,13 @@ function AboutUsPage({ t }) {
         <div className="absolute" style={{ top: '65vw', left: '3vw' }}>
           <AboutGraph />
         </div>
-        <div className="absolute " style={{ top: '190vw', right: '0' }}>
-          {t('aboutus.our_team')}
-        </div>
         <div
-          className="absolute "
-          style={{ top: '188vw', right: '0', left: '0' }}
+          className="absolute text-white text-2xl font-medium"
+          style={{ top: '190vw', left: '65vw' }}
         >
+          OUR TEAM
+        </div>
+        <div className="absolute " style={{ top: '190vw', right: '0' }}>
           <TeamCircles />
         </div>
         <div
@@ -49,4 +47,4 @@ function AboutUsPage({ t }) {
   );
 }
 
-export default withNamespaces()(AboutUsPage);
+export default AboutUsPage;
